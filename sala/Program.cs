11a68a846e6b72ba21860d8b781efe28c9051333
WillianYamamoto <willian.yamamoto@mottu.com.br>
2025-05-27 06:48:33 -0300

    Passo1

--- sala/Program.cs
+++ sala/Program.cs
@@ -1,2 +1,5 @@
 ﻿//Desenvolva uma aplicação REST em .NET que simule a gestão de salas de reunião em um ambiente corporativo. O sistema permitirá o cadastro de salas e a realização de reservas, com algumas regras de negócio. Não é necessário utilizar banco de dados ou armazenar os dados em memória entre requisições.
 //O foco desta avaliação é a aplicação dos pilares da orientação a objetos dentro de uma arquitetura em camadas e a exposição de funcionalidades básicas via API RESTful.
+//Crie listas locais temporárias dentro dos serviços ou controllers (se necessário).
+//Use DateTime.AddHours() para comparar reservas.
+//Retorne mensagens claras com BadRequest quando necessário.
